8a22ba9b02c48816fcd8b98dc369d344c3d17d38
Phillip Brown <nzjeux18@hotmail.co.nz>
2022-06-19 20:54:42 +1200

    Part way through part two

--- part1/include/cpu.h
+++ part1/include/cpu.h
@@ -1,6 +1,34 @@
 #pragma once
 
 #include <common.h>
+#include <instructions.h>
+typedef struct 
+{
+    u8 a;
+    u8 f;
+    u8 b;
+    u8 c;
+    u8 d;
+    u8 e;
+    u8 h;
+    u8 l;
+    u16 pc;
+    u16 sp;   
+} cpu_registers; //CPU registers
+
+typedef struct //what the CPU is doing
+{
+    cpu_registers regs;
+
+    u16 fetch_data;
+    u16 mem_dest;
+    u16 cur_opcode;
+    instruction *cur_inst;
+
+    bool halted;
+    bool stepping;
+} cpu_contex;
+
 
 void cpu_init();
 bool cpu_step();
---
+++ part1/lib/instructions.c
@@ -0,0 +1,9 @@
+#include <instructions.h>
+#include <cpu.h>
+
+instruction instructions[0x100] = {
+    [0x00] = {IN_NOP, AM_IMP},
+
+
+    
+};
